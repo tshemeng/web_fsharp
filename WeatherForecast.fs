6b2998cb072533829c00dd6dc8de15721491824b
mengzhao <tshemeng@users.noreply.github.com>
2022-07-18 15:25:02 +0800

    feat:  init

---
+++ WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace web_fsharp
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32.0 + (float this.TemperatureC / 0.5556)
